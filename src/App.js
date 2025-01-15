43ad9c00ffc98a9d48575448d262fedd6595bc85
SeongBeom <vnvnwn@gmail.com>
2025-01-15 23:56:53 +0900

    modal창

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   let post = '강남 우동 맛집1';
   let [title, titleChange] = useState(['남자코트 추천', '강남 우동맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
+
 
   return (
     <div className="App">
@@ -33,11 +35,48 @@ function App() {
         <p>1월 7일 발행</p>
       </div>
       <div className = "list">
-        <h4>{ title[2] }</h4>
+        <h4 onClick={ ()=> { 
+          if(modal == false){
+            setModal(true);
+          }else{
+            setModal(false);
+          }
+        }}>{ title[2] }</h4>
         <p>1월 7일 발행</p>
       </div>
+
+        {
+          modal == true ? <Modal/> : null
+        }
+      <Modal/>
+      {/* <Study/> */}
+      
     </div>
   );
 }
 
+// 컴포넌트 쓰면 좋을때 1 - 반복적인 html 축약 시
+// 2 - 큰 페이지 하나를 만들 수 있음
+// 3 - 자주 변경되는 것들
+function Modal(){
+  return(
+    <div className='modal'>
+      <h4>제목1</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+     
+  )
+}
+
+function Study(){
+  return(
+    <div>
+      <span>아무거나</span>
+
+    </div>
+
+
+  )
+}
 export default App;
